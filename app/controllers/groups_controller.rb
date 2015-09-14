5a1f7b17d191bfa8efbaf970ed80457517ae987e
LiouMichael <lioumichael@airbears2-10-142-49-75.airbears2.1918.berkeley.edu>
2015-09-14 15:48:23 -0700

    finished chapter 3

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
   def index
+    flash[:notice] = "good morning!"
+    flash[:alert] = "time to sleep!"
+    flash[:warning] = "warning message!"
   end
 end
